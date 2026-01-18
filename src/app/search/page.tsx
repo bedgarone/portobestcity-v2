2be9240e0d12c45b1770b081e133a6a3a033c063
Bruno Oliveira <brunoedgarero@gmail.com>
2026-01-18 21:26:46 +0000

    feat: metadata + protagonists category with interview articles + search page desogn

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -3,8 +3,9 @@ import { Post } from '@/app/types'
 import { SearchBar } from '@/components/SearchBar'
 import PostsList from '@/components/PostsList'
 import { Suspense } from 'react'
+import { MAIN_CONTAINER_CLASSES } from '@/app/utils'
 
-const SEARCH_QUERY = `*[_type == "post" && (title match $searchQuery || pt::text(body) match $searchQuery)] | order(publishedAt desc) {_id, title, slug, publishedAt}`
+const SEARCH_QUERY = `*[_type == "post" && (title match $searchQuery || pt::text(body) match $searchQuery)]{...,author->,category->} | order(publishedAt desc)`
 
 export default async function SearchPage({ searchParams }: { searchParams: Promise<{ q?: string }> }) {
   const { q } = await searchParams
@@ -18,18 +19,18 @@ export default async function SearchPage({ searchParams }: { searchParams: Promi
   }
 
   return (
-    <main className="container mx-auto min-h-screen max-w-3xl p-8">
+    <main className={MAIN_CONTAINER_CLASSES}>
       <header className="flex flex-col gap-4">
-        <h1 className="text-4xl font-bold">Search News</h1>
+        <h1 className="text-dark-blue text-center font-sans text-2xl font-medium tracking-wide uppercase">Search</h1>
         <Suspense>
-          <SearchBar />
+          <SearchBar border />
         </Suspense>
       </header>
 
-      <section>
+      <section className="mt-4 font-sans">
         {query ? (
           <div>
-            <h2 className="mb-6 text-2xl font-semibold">{`Results for "${query}"`}</h2>
+            <h2 className="text-dark-blue mb-6">{`Results for "${query}":`}</h2>
             {posts.length > 0 ? <PostsList posts={posts} /> : <p>No results found.</p>}
           </div>
         ) : (
